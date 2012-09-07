9da41819be07e12245006264560a9086d858af76
Ric Allinson <capecodehq@yahoo.co.uk>
2012-09-06 21:31:29 -0700

    Idea 1

--- cms/init.js
+++ cms/init.js
@@ -21,32 +21,6 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-/*jslint nomen: true, stupid: true*/
-
 "use strict";
 
-var program = require('commander'),
-    path = require('path'),
-    fs = require('fs'),
-    commandsDir = path.join(__dirname, 'commands');
-
-// Set the version
-program.version('0.0.0');
-program.option("-v, --verbose", "runtime info");
-
-require("ash/lib/commands/start");
-
-// Add all the local commands
-fs.readdirSync(commandsDir).forEach(function (filename) {
-    if (!/\.js$/.test(filename)) {
-        return;
-    }
-    require(path.join(commandsDir, filename));
-});
-
-// If we have no arguments show the help
-if (process.argv.length <= 2) {
-    process.argv.push('--help');
-}
-
-program.parse(process.argv);
+require(["lib/adapters/dom"]);
